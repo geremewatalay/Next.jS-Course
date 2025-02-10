79e2f5da61e6d2e6a7169b490eeb4c6ca2048fc8
geremewatalay <geremewataly@gmail.com>
2025-02-10 04:23:49 -0800

    active link

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title:{
-    absolute:"",
+    
     default:"Next.js Tutorial - AhaduTech",
     template:"%s | AhaduTech",
   },
